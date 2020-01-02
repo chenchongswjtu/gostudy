ce3cc1009a51dd103e90c5008d5cdb1962ebeea7
chenchongswjtu <1499760048@qq.com>
2020-01-02 13:46:06 +0800

    modify

--- convert/convert.go
+++ convert/convert.go
@@ -27,4 +27,27 @@ func main() {
 	// int64 to string
 	s = strconv.FormatInt(int64(67), 10)
 	fmt.Println(s)
+
+	// uin664 to string
+	fmt.Println(strconv.FormatUint(34, 10))
+
+	// float64 to string
+	fmt.Println(strconv.FormatFloat(123.66, 'f', 6, 64))
+
+	// float32 to string
+	fmt.Println(strconv.FormatFloat(123.66, 'f', -1, 32))
+
+	// string to float32
+	f32, err := strconv.ParseFloat("44.3", 32)
+	if err != nil {
+		fmt.Println("string to float32 err:", err)
+	}
+	fmt.Println(f32)
+
+	// string to float64
+	f64, err := strconv.ParseFloat("44.3", 64)
+	if err != nil {
+		fmt.Println("string to float32 err:", err)
+	}
+	fmt.Println(f64)
 }
