d8de38c28c0155fcce99053171233b37de374205
chenchong <1499760048@qq.com>
2021-04-14 14:03:46 +0800

    add

--- myleetcode/二叉树/main.go
+++ myleetcode/二叉树/main.go
@@ -426,3 +426,48 @@ func isBalanced(root *TreeNode) bool {
 
 	return isBalanced(root.Left) && isBalanced(root.Right)
 }
+
+// 最小高度
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right != nil {
+		return minDepth(root.Right) + 1
+	}
+
+	if root.Right == nil && root.Left != nil {
+		return minDepth(root.Left) + 1
+	}
+
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+//func minDepth(root *TreeNode) int {
+//	if root == nil {
+//		return 0
+//	}
+//
+//	if root.Left == nil && root.Right == nil {
+//		return 1
+//	}
+//
+//	minD := math.MaxInt64
+//	if root.Left != nil {
+//		minD = min(minD, minDepth(root.Left))
+//	}
+//
+//	if root.Right != nil {
+//		minD = min(minD, minDepth(root.Right))
+//	}
+//
+//	return minD + 1
+//}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
