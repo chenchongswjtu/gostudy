99f21e2f8c44608cca36e1aa50c9ec361c507a08
chenchong <1499760048@qq.com>
2021-04-22 13:46:21 +0800

    add

--- myleetcode/二叉树/tree.go
+++ myleetcode/二叉树/tree.go
@@ -1239,3 +1239,28 @@ func tree2str(t *TreeNode) string {
 
 	return strconv.Itoa(t.Val) + "(" + tree2str(t.Left) + ")(" + tree2str(t.Right) + ")"
 }
+
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil && root2 == nil {
+		return nil
+	}
+
+	if root1 == nil {
+		return root2
+	}
+
+	if root2 == nil {
+		return root1
+	}
+
+	root := &TreeNode{
+		Val:   root1.Val + root2.Val,
+		Left:  nil,
+		Right: nil,
+	}
+
+	root.Left = mergeTrees(root1.Left, root2.Left)
+	root.Right = mergeTrees(root1.Right, root2.Right)
+
+	return root
+}
