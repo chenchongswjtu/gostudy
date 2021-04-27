e0e009e69161770f94bdabfbb8463b6f94166cd1
chenchong <1499760048@qq.com>
2021-04-27 16:53:49 +0800

    22. 括号生成

--- myleetcode/回溯/main.go
+++ myleetcode/回溯/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println(letterCombinations("23"))
+	fmt.Println(generateParenthesis1(3))
 }
 
 // 17. 电话号码的字母组合
@@ -41,5 +41,73 @@ func letterCombinationsHelper(letter map[string][]string, digits string, n int, 
 		s += v // 选择
 		letterCombinationsHelper(letter, digits, n+1, s, ans)
 		s = t // 撤销选择，回溯
+	}
+}
+
+// 22. 括号生成
+// 暴力解法
+func generateParenthesis(n int) []string {
+	var ans []string
+	genAll("", n, &ans)
+	return ans
+}
+
+func genAll(s string, n int, ans *[]string) {
+	if len(s) == 2*n {
+		if isValid(s) {
+			*ans = append(*ans, s)
+		}
+		return
+	}
+	genAll(s+"(", n, ans)
+	genAll(s+")", n, ans)
+
+}
+
+func isValid(s string) bool {
+	balance := 0
+	for _, c := range s {
+		if c == '(' {
+			balance++
+		}
+
+		if c == ')' {
+			balance--
+		}
+
+		if balance < 0 {
+			return false
+		}
+	}
+
+	return balance == 0
+}
+
+// 22. 括号生成
+// 回溯解法
+func generateParenthesis1(n int) []string {
+	var ans []string
+	backtrack("", 0, 0, n, &ans)
+	return ans
+}
+
+func backtrack(cur string, l int, r int, n int, ans *[]string) {
+	if len(cur) == 2*n {
+		*ans = append(*ans, cur)
+		return
+	}
+
+	if l < n {
+		t := cur
+		cur += "("
+		backtrack(cur, l+1, r, n, ans)
+		cur = t
+	}
+
+	if r < l {
+		t := cur
+		cur += ")"
+		backtrack(cur, l, r+1, n, ans)
+		cur = t
 	}
 }
