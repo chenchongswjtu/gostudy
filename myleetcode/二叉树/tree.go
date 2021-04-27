38ad5010c47aed731af4887b0e178b0aa96a369b
chenchong <1499760048@qq.com>
2021-04-27 14:31:41 +0800

    701. 二叉搜索树中的插入操作

--- myleetcode/二叉树/tree.go
+++ myleetcode/二叉树/tree.go
@@ -1619,3 +1619,22 @@ func searchBST(root *TreeNode, val int) *TreeNode {
 
 	return searchBST(root.Left, val)
 }
+
+// 701. 二叉搜索树中的插入操作
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val, Left: nil, Right: nil}
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if root.Val < val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	return root
+}
