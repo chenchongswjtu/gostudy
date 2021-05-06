ad20a4eda7d3fb63e532f56b825a1ca6915497fe
chenchong <1499760048@qq.com>
2021-05-06 15:20:35 +0800

    842. 将数组拆分成斐波那契序列

--- myleetcode/回溯/main.go
+++ myleetcode/回溯/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	fmt.Println(allPathsSourceTarget([][]int{{4, 3, 1}, {3, 2, 4}, {3}, {4}, {}}))
+	fmt.Println(splitIntoFibonacci("112358"))
 }
 
 // 17. 电话号码的字母组合
@@ -841,5 +842,52 @@ func allPathsSourceTargetHelper(graph [][]int, n int, i int, one []int, all *[][
 		one = t
 	}
 
+	return
+}
+
+// 842. 将数组拆分成斐波那契序列
+func splitIntoFibonacci(s string) (f []int) {
+	n := len(s)
+	var backtrack func(index, sum, prev int) bool
+	backtrack = func(index, sum, prev int) bool {
+		if index == n {
+			return len(f) >= 3
+		}
+
+		cur := 0
+		for i := index; i < n; i++ {
+			// 每个块的数字一定不要以零开头，除非这个块是数字 0 本身
+			if i > index && s[index] == '0' {
+				break
+			}
+
+			cur = cur*10 + int(s[i]-'0')
+			// 拆出的整数要符合 32 位有符号整数类型
+			if cur > math.MaxInt32 {
+				break
+			}
+
+			// F[i] + F[i+1] = F[i+2]
+			if len(f) >= 2 {
+				if cur < sum {
+					continue
+				}
+
+				if cur > sum {
+					break
+				}
+			}
+
+			// cur 符合要求，加入序列 F
+			f = append(f, cur)
+			if backtrack(i+1, prev+cur, cur) {
+				return true
+			}
+			// 后面不符合
+			f = f[:len(f)-1]
+		}
+		return false
+	}
+	backtrack(0, 0, 0)
 	return
 }
