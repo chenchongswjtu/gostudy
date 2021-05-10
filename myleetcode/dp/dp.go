4b4675839d890f087f97298662fa182c4a1903d2
chenchong <1499760048@qq.com>
2021-05-10 17:16:28 +0800

    62. 不同路径

--- myleetcode/dp/dp.go
+++ myleetcode/dp/dp.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println(longestPalindrome1("abab"))
+	fmt.Println(uniquePaths(7, 3))
 }
 
 // 5. 最长回文子串
@@ -145,4 +145,35 @@ func maxSubArray1(nums []int) int {
 		}
 	}
 	return max
+}
+
+// 62. 不同路径
+func uniquePaths(m int, n int) int {
+	if m == 0 || n == 0 {
+		return 0
+	}
+	if m == 1 || n == 1 {
+		return 1
+	}
+
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
 }
