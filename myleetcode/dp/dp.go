cf13d70e8dd8835ff8e2ee2588c9f54e128ef7f4
chenchong <1499760048@qq.com>
2021-05-11 22:38:17 +0800

    131. 分割回文串

--- myleetcode/dp/dp.go
+++ myleetcode/dp/dp.go
@@ -449,3 +449,42 @@ func maxProfit4(k int, prices []int) int {
 
 	return dp[n-1][k][0]
 }
+
+// 131. 分割回文串
+func partition(s string) [][]string {
+	var ans [][]string
+	n := len(s)
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+		for j := range dp[i] {
+			dp[i][j] = true
+		}
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			dp[i][j] = s[i] == s[j] && dp[i+1][j-1]
+		}
+	}
+
+	ss := make([]string, 0)
+	var dfs func(int)
+	dfs = func(i int) {
+		if i == n {
+			ans = append(ans, append([]string(nil), ss...))
+			return
+		}
+
+		for j := i; j < n; j++ {
+			if dp[i][j] {
+				ss = append(ss, s[i:j+1])
+				dfs(j + 1)
+				ss = ss[:len(ss)-1]
+			}
+		}
+	}
+
+	dfs(0)
+	return ans
+}
