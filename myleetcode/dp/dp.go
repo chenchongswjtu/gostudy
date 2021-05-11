3125b0cd4d6d87d77910c2c237a37b9dadb23b5e
chenchong <1499760048@qq.com>
2021-05-11 11:29:26 +0800

    97. 交错字符串

--- myleetcode/dp/dp.go
+++ myleetcode/dp/dp.go
@@ -258,3 +258,30 @@ func minPathSum(grid [][]int) int {
 
 	return dp[m-1][n-1]
 }
+
+// 97. 交错字符串
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	n1, n2, n3 := len(s1), len(s2), len(s3)
+	if n1+n2 != n3 {
+		return false
+	}
+
+	f := make([][]bool, n1+1)
+	for i := 0; i <= n1; i++ {
+		f[i] = make([]bool, n2+1)
+	}
+	f[0][0] = true
+	for i := 0; i <= n1; i++ {
+		for j := 0; j <= n2; j++ {
+			k := i + j - 1
+			if i > 0 {
+				f[i][j] = f[i][j] || (f[i-1][j] && s1[i-1] == s3[k])
+			}
+			if j > 0 {
+				f[i][j] = f[i][j] || (f[i][j-1] && s2[j-1] == s3[k])
+			}
+		}
+	}
+
+	return f[n1][n2]
+}
