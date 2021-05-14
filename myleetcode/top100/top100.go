88cd38942f7c1d5da293de6c5fa50690cf73ec5c
chenchong <1499760048@qq.com>
2021-05-14 16:28:00 +0800

    33. 搜索旋转排序数组

--- myleetcode/top100/top100.go
+++ myleetcode/top100/top100.go
@@ -422,3 +422,33 @@ func nextPermutation(nums []int) {
 		y--
 	}
 }
+
+// 33. 搜索旋转排序数组
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[len(nums)-1] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return -1
+}
