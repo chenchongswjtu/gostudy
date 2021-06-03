38afee6c9869788c34ca972fdce77683c4783b1d
chenchong <1499760048@qq.com>
2021-06-03 16:41:30 +0800

    200. 岛屿数量

--- myleetcode/top100/top100.go
+++ myleetcode/top100/top100.go
@@ -855,3 +855,37 @@ func reverseList(head *ListNode) *ListNode {
 
 	return pre
 }
+
+// 200. 岛屿数量
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	x, y := len(grid), len(grid[0])
+	ans := 0
+
+	var dfs func(i, j int)
+	dfs = func(i, j int) {
+		if i < 0 || i >= x || j < 0 || j >= y || grid[i][j] == '0' {
+			return
+		}
+
+		grid[i][j] = '0'
+		dfs(i+1, j)
+		dfs(i-1, j)
+		dfs(i, j-1)
+		dfs(i, j+1)
+	}
+
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			if grid[i][j] == '1' {
+				ans++
+				dfs(i, j)
+			}
+		}
+	}
+
+	return ans
+}
