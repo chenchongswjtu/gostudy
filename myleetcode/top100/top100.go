92a7f7ef67c9eade2117f4a5dcf20ffef20bf2de
chenchong <1499760048@qq.com>
2021-06-08 16:52:24 +0800

    309. 最佳买卖股票时机含冷冻期

--- myleetcode/top100/top100.go
+++ myleetcode/top100/top100.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println(coinChange([]int{2}, 3))
+	fmt.Println(maxProfit([]int{1, 2, 4}))
 }
 
 // 3. 无重复字符的最长子串(滑动窗口)
@@ -973,4 +973,26 @@ func coinChange(coins []int, amount int) int {
 		return -1
 	}
 	return dp[amount]
+}
+
+// 309. 最佳买卖股票时机含冷冻期
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	n := len(prices)
+	// dp[i][0]: 手上持有股票的最大收益
+	// dp[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+	// dp[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+	dp := make([][3]int, n)
+	dp[0][0] = -prices[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][2]-prices[i]) // 不做，上一次不在冷冻期买股票
+		dp[i][1] = dp[i-1][0] + prices[i]                // 前一天有股票再买
+		dp[i][2] = max(dp[i-1][2], dp[i-1][1])           // 前一天不在冷冻期不做，前一天是在冷冻期
+
+	}
+
+	return max(dp[n-1][1], dp[n-1][2])
 }
