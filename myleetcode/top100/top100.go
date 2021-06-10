92fa2d1529918760c6e0f90715964d308d2c450d
chenchong <1499760048@qq.com>
2021-06-10 19:31:20 +0800

    739. 每日温度 (单调栈)

--- myleetcode/top100/top100.go
+++ myleetcode/top100/top100.go
@@ -1302,3 +1302,20 @@ func subarraySum(nums []int, k int) int {
 	}
 	return count
 }
+
+// 739. 每日温度 (单调栈)
+func dailyTemperatures(temperatures []int) []int {
+	length := len(temperatures)
+	ans := make([]int, length)
+	var stack []int
+	for i := 0; i < length; i++ {
+		temperature := temperatures[i]
+		for len(stack) > 0 && temperature > temperatures[stack[len(stack)-1]] {
+			prevIndex := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			ans[prevIndex] = i - prevIndex
+		}
+		stack = append(stack, i)
+	}
+	return ans
+}
