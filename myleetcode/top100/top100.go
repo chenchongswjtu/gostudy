9140824d26f030cbb28d6c7edc3603b8b0382453
chenchong <1499760048@qq.com>
2021-06-10 17:11:19 +0800

    560. 和为K的子数组

--- myleetcode/top100/top100.go
+++ myleetcode/top100/top100.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	fmt.Println(reconstructQueue([][]int{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}}))
+	fmt.Println(subarraySum([]int{1, 1, 1}, 2))
 }
 
 // 3. 无重复字符的最长子串(滑动窗口)
@@ -1271,4 +1271,34 @@ func findTargetSumWays(nums []int, target int) (count int) {
 	}
 	backtrack(0, 0)
 	return
+}
+
+// 560. 和为K的子数组
+func subarraySum1(nums []int, k int) int {
+	count := 0
+	for start := 0; start < len(nums); start++ {
+		sum := 0
+		for end := start; end >= 0; end-- {
+			sum += nums[end]
+			if sum == k {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+// 560. 和为K的子数组
+func subarraySum(nums []int, k int) int {
+	count, pre := 0, 0
+	m := map[int]int{}
+	m[0] = 1
+	for i := 0; i < len(nums); i++ {
+		pre += nums[i]
+		if v, ok := m[pre-k]; ok {
+			count += v
+		}
+		m[pre] += 1
+	}
+	return count
 }
