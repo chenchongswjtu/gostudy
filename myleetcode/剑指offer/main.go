d63232a5d190c9a2c31af23bf77e5b1e8d08d9fb
chenchong <1499760048@qq.com>
2021-06-16 17:22:47 +0800

    剑指 Offer 04. 二维数组中的查找

---
+++ myleetcode/剑指offer/main.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+
+}
+
+// 剑指 Offer 04. 二维数组中的查找
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	m := len(matrix)
+	if m == 0 {
+		return false
+	}
+	n := len(matrix[0])
+	if n == 0 {
+		return false
+	}
+
+	i, j := 0, n-1
+	for i >= 0 && i <= m-1 && j >= 0 && j <= n-1 {
+		if matrix[i][j] == target {
+			return true
+		}
+		if matrix[i][j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return false
+}
