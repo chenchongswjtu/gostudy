be0c010c96285c3027a71cf9ad1a5fac9357d909
chenchong <1499760048@qq.com>
2021-07-19 10:51:55 +0800

    剑指 Offer 63. 股票的最大利润

--- myleetcode/剑指offer/main.go
+++ myleetcode/剑指offer/main.go
@@ -1204,3 +1204,34 @@ func isStraight(nums []int) bool {
 
 	return count >= 0
 }
+
+// 剑指 Offer 63. 股票的最大利润
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
+	res := 0
+	m := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		m = min(m, prices[i-1])
+		res = max(res, prices[i]-m)
+	}
+
+	return res
+}
