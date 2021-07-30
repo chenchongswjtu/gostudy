5a748b83162ebc1c26bc5ca2ff41c5e0d2241f9e
chenchong <1499760048@qq.com>
2021-07-30 16:45:19 +0800

    11231302. 层数最深叶子节点的和

--- myleetcode/二叉树/tree.go
+++ myleetcode/二叉树/tree.go
@@ -2373,3 +2373,52 @@ func lcaDeepestLeaves(root *TreeNode) *TreeNode {
 		return root
 	}
 }
+
+// 1302. 层数最深叶子节点的和
+func deepestLeavesSum(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	var maxDepth int
+	var sum int
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	var getDepth func(node *TreeNode) int
+	getDepth = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		return max(getDepth(node.Left), getDepth(node.Right)) + 1
+	}
+
+	maxDepth = getDepth(root)
+	var dfs func(node *TreeNode, depth int)
+	dfs = func(node *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+
+		if node.Left == nil && node.Right == nil {
+			if depth == maxDepth {
+				sum += node.Val
+			}
+			return
+		}
+
+		if node.Left != nil {
+			dfs(node.Left, depth+1)
+		}
+
+		if node.Right != nil {
+			dfs(node.Right, depth+1)
+		}
+	}
+
+	dfs(root, 1)
+	return sum
+}
