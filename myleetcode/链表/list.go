3be964108f61866da0965df43830b790b1329232
chenchong <1499760048@qq.com>
2021-08-16 10:32:43 +0800

    143. 重排链表

--- myleetcode/链表/list.go
+++ myleetcode/链表/list.go
@@ -447,3 +447,25 @@ func reverseAB1(a, b *ListNode) *ListNode {
 
 	return pre
 }
+
+// 143. 重排链表
+func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+	var nodes []*ListNode
+	for node := head; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
+	i, j := 0, len(nodes)-1
+	for i < j {
+		nodes[i].Next = nodes[j]
+		i++
+		if i == j {
+			break
+		}
+		nodes[j].Next = nodes[i]
+		j--
+	}
+	nodes[i].Next = nil
+}
