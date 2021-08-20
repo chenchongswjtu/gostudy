d6943cb79fb5e38cba5a30e461d01867af92086f
chenchong <1499760048@qq.com>
2021-08-20 11:22:29 +0800

    无内存分配字节数组转字符串

---
+++ 无内存分配字节数组转字符串/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func main() {
+	b := []byte("sss")
+	s := BytesToString(b)
+	fmt.Println(unsafe.Pointer(&b), unsafe.Pointer(&s))
+}
+
+// BytesToString converts byte slice to a string without memory allocation.
+//
+// Note it may break if the implementation of string or slice header changes in the future go versions.
+func BytesToString(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// StringToBytes converts string to a byte slice without memory allocation.
+//
+// Note it may break if the implementation of string or slice header changes in the future go versions.
+func StringToBytes(s string) (b []byte) {
+	/* #nosec G103 */
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+
+	bh.Data, bh.Len, bh.Cap = sh.Data, sh.Len, sh.Len
+	return b
+}
