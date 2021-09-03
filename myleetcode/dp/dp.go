4bf0d50259a99df7a043b108ea3aa239fbc2513d
chenchong <1499760048@qq.com>
2021-09-03 09:42:04 +0800

    获得goroutine id

--- myleetcode/dp/dp.go
+++ myleetcode/dp/dp.go
@@ -135,7 +135,7 @@ func maxSubArray1(nums []int) int {
 	dp[0] = nums[0]
 	max := dp[0]
 	for i := 1; i < len(nums); i++ {
-		if dp[i-1]+nums[i] > nums[i] {
+		if dp[i-1] > 0 {
 			dp[i] = dp[i-1] + nums[i]
 		} else {
 			dp[i] = nums[i]
