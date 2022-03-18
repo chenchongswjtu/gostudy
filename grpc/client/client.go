727725942173098474106fbdb0140c1333ac0798
chenchong <1499760048@qq.com>
2022-03-18 09:56:44 +0800

    grpc 流式与飞流式性能测试

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"strconv"
 	"time"
 
 	"google.golang.org/grpc"
@@ -21,32 +22,77 @@ func main() {
 	client := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
-	name := "world"
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	// name := "world"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 	// 3.通过客户端远程调用函数
-	r, err := client.SayHello(ctx, &pb.HelloRequest{Name: name})
+	var i int
+	var t1 = time.Now().UnixNano()
+
+	//for {
+	//	i++
+	//	r, err := client.SayHello(ctx, &pb.HelloRequest{Name: strconv.Itoa(i)})
+	//	if err != nil {
+	//		log.Fatalf("unary send: %v", err)
+	//	}
+	//	log.Printf("Greeting: %s", r.GetMessage())
+	//	if i == 10000 {
+	//		log.Printf("cost %v\n", time.Duration(time.Now().UnixNano()-t1))
+	//		break
+	//	}
+	//}
+
+	var stream pb.Greeter_HelloStreamClient
+
+	stream, err = client.HelloStream(ctx)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
 
-	stream, err := client.HelloStream(ctx)
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+	for {
+		i++
+
+		err = stream.Send(&pb.HelloRequest{Name: strconv.Itoa(i)})
+		if err != nil {
+			log.Fatalf("stream send %s", err)
+		}
+
+		r, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("client recv err %s", err)
+		}
+
+		log.Printf("Greeting: %s", r.GetMessage())
+		if i == 10000 {
+			log.Printf("cost %v\n", time.Duration(time.Now().UnixNano()-t1))
+			break
+		}
 	}
 
-	err = stream.Send(&pb.HelloRequest{Name: "sss"})
-	if err != nil {
-		log.Fatalf("2222 %s", err)
-	}
+	_ = stream.CloseSend()
 
-	recv, err := stream.Recv()
-	if err != nil {
-		log.Fatalf("3333 %s", err)
-	}
-
-	log.Println("client", recv.GetMessage())
-
-	stream.CloseSend()
+	//r, err := client.SayHello(ctx, &pb.HelloRequest{Name: name})
+	//if err != nil {
+	//	log.Fatalf("could not greet: %v", err)
+	//}
+	//log.Printf("Greeting: %s", r.GetMessage())
+	//
+	//stream, err := client.HelloStream(ctx)
+	//if err != nil {
+	//	log.Fatalf("could not greet: %v", err)
+	//}
+	//
+	//err = stream.Send(&pb.HelloRequest{Name: "sss"})
+	//if err != nil {
+	//	log.Fatalf("2222 %s", err)
+	//}
+	//
+	//recv, err := stream.Recv()
+	//if err != nil {
+	//	log.Fatalf("3333 %s", err)
+	//}
+	//
+	//log.Println("client", recv.GetMessage())
+	//
+	//stream.CloseSend()
 }
