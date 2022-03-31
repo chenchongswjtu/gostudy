82dffbdf745f1b4066e3c8555c94eafe4e4b2032
chenchong <1499760048@qq.COM>
2022-03-31 13:42:47 +0800

    215. 数组中的第K个最大元素

---
+++ myleetcode/hot100/215. 数组中的第K个最大元素.go
@@ -0,0 +1,46 @@
+package main
+
+import "container/heap"
+
+// 使用小根堆
+// 还可以使用二分排序查找第n-k个数
+
+//215. 数组中的第K个最大元素
+func findKthLargest(nums []int, k int) int {
+	var h = &minHeap{}
+	heap.Init(h)
+	for _, num := range nums {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	return heap.Pop(h).(int)
+}
+
+type minHeap []int
+
+func (h minHeap) Len() int {
+	return len(h)
+}
+
+func (h minHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h minHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *minHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *minHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
---
+++ myleetcode/hot100/215. 数组中的第K个最大元素_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFindKthLargest(t *testing.T) {
+	fmt.Println(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+}
