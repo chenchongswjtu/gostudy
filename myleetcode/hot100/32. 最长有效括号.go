bad74756fee39dd8b23492f0214f449a7cc61689
chenchong <1499760048@qq.COM>
2022-04-12 17:28:39 +0800

    32. 最长有效括号

--- myleetcode/hot100/32. 最长有效括号.go
+++ myleetcode/hot100/32. 最长有效括号.go
@@ -60,3 +60,34 @@ func longestValidParentheses(s string) int {
 	}
 	return ret
 }
+
+// 这个方法比较好理解
+func longestValidParentheses1(s string) int {
+	left, right, maxLength := 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			maxLength = max(maxLength, 2*right)
+		} else if right > left {
+			left, right = 0, 0
+		}
+	}
+	left, right = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			maxLength = max(maxLength, 2*left)
+		} else if left > right {
+			left, right = 0, 0
+		}
+	}
+	return maxLength
+}
