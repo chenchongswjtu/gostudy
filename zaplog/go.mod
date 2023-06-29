08311ab736e531b7c67f1719bb01316a01f41960
chenc299 <chenc299@chinaunicom.cn>
2023-06-29 15:39:51 +0800

    zap log 自定义日志格式输出

--- zaplog/go.mod
+++ zaplog/go.mod
@@ -1,8 +1,9 @@
 module github.com/chenchong/zaplog
 
-go 1.20
+go 1.19
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	go.uber.org/zap v1.24.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
--- zaplog/zaplog/log.go
+++ zaplog/zaplog/log.go
@@ -2,19 +2,78 @@ package zaplog
 
 import (
 	"os"
+	"strings"
+	"time"
 
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
+
+const logTmFmtWithMS = "2006-01-02 15:04:05.000"
 
 var L = NewLogger()
 
 func NewLogger() *zap.SugaredLogger {
 	coreList := make([]zapcore.Core, 0)
 
-	encoderConfig := zap.NewProductionEncoderConfig()     //NewJSONEncoder()输出json格式，NewConsoleEncoder()输出普通文本格式
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder //指定时间格式
+	// 自定义时间输出格式
+	customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + t.Format(logTmFmtWithMS) + "]")
+	}
+	// 自定义日志级别显示
+	customLevelEncoder := func(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + level.CapitalString() + "]")
+	}
+
+	// 自定义文件：行号输出项
+	customCallerEncoder := func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + uuid.NewString() + "]")
+		enc.AppendString("[" + caller.TrimmedPath() + "]")
+	}
+
+	customNameEncoder := func(loggerName string, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + strings.ToUpper(loggerName) + "]")
+	}
+
+	// [2023-06-29 15:35:12.313]       [DEBUG] [TEST]  [af378fc1-5d11-4bc4-a454-6e7352969bc8]  [cmd/main.go:15]        this is debug
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = customTimeEncoder
+	encoderConfig.EncodeLevel = customLevelEncoder
+	encoderConfig.EncodeCaller = customCallerEncoder
+	encoderConfig.EncodeName = customNameEncoder
+	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
+
+	//encoderConfig := zapcore.EncoderConfig{
+	//	CallerKey:      "caller_line", // 打印文件名和行数
+	//	LevelKey:       "level_name",
+	//	MessageKey:     "msg",
+	//	TimeKey:        "ts",
+	//	NameKey:        "name",
+	//	StacktraceKey:  "stacktrace",
+	//	LineEnding:     zapcore.DefaultLineEnding,
+	//	EncodeTime:     customTimeEncoder,   // 自定义时间格式
+	//	EncodeLevel:    customLevelEncoder,  // 小写编码器
+	//	EncodeCaller:   customCallerEncoder, // 全路径编码器
+	//	EncodeDuration: zapcore.SecondsDurationEncoder,
+	//	//EncodeName:     zapcore.FullNameEncoder,
+	//	EncodeName: customNameEncoder, // 自定义
+	//}
+
+	//// level大写染色编码器
+	//if l.enableColor {
+	//	encoderConf.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	//}
+	//
+	//// json 格式化处理
+	//if l.jsonFormat {
+	//	return zapcore.NewCore(zapcore.NewJSONEncoder(encoderConf),
+	//		syncWriter, zap.NewAtomicLevelAt(l.logMinLevel))
+	//}
+
+	//encoderConfig := zap.NewProductionEncoderConfig()     //NewJSONEncoder()输出json格式，NewConsoleEncoder()输出普通文本格式
+	//encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder //指定时间格式
 	//encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder //按级别显示不同颜色，不需要的话取值zapcore.CapitalLevelEncoder就可以了
 	//encoderConfig.EncodeCaller = zapcore.FullCallerEncoder //显示完整文件路径
 	encoder := zapcore.NewConsoleEncoder(encoderConfig)
